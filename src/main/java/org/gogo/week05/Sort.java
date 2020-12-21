3869a2037cc603af7b6a915774a2e27d0dabe324
Redick01 <11090829@qq.com>
2020-12-22 00:27:04 +0800

    add quick sort

---
+++ src/main/java/org/gogo/week05/Sort.java
@@ -0,0 +1,33 @@
+package org.gogo.week05;
+
+/**
+ * @author liupenghui
+ * @date 2020/12/22 12:22 上午
+ */
+public class Sort {
+
+    public static void quickSort(int[] n,int left,int right) {
+        int dp;
+        if (left < right) {
+            dp = partition(n, left, right);
+            quickSort(n, left, dp - 1);
+            quickSort(n, dp + 1, right);
+        }
+    }
+
+    public static int partition(int n[], int left, int right) {
+        int pivot = n[left];
+        while (left < right) {
+            while (left < right && n[right] >= pivot)
+                right--;
+            if (left < right)
+                n[left++] = n[right];
+            while (left < right && n[left] <= pivot)
+                left++;
+            if (left < right)
+                n[right--] = n[left];
+        }
+        n[left] = pivot;
+        return left;
+    }
+}
