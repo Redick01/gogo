92d95fe530ea5ab1ffabe69609b6612f1b95d898
Redick01 <11090829@qq.com>
2020-12-30 00:07:56 +0800

    commit counting sort

--- src/main/java/org/gogo/week05/Sort.java
+++ src/main/java/org/gogo/week05/Sort.java
@@ -6,6 +6,12 @@ package org.gogo.week05;
  */
 public class Sort {
 
+    /**
+     * 快速排序
+     * @param n
+     * @param left
+     * @param right
+     */
     public static void quickSort(int[] n,int left,int right) {
         int dp;
         if (left < right) {
@@ -31,5 +37,29 @@ public class Sort {
         return left;
     }
 
-
+    public static int[] countingSort(int[] n) {
+        // 找到待排序数组中最大值
+        int max = Integer.MIN_VALUE;
+        for (int a : n) {
+            max = Math.max(max, a);
+        }
+        // 创建中间数组，数组长度最大值+1
+        int[] count = new int[max + 1];
+        for (int a : n) {
+            count[a]++;
+        }
+        // 创建结果数组
+        int[] result = new int[n.length];
+        // 创建结果数组的起始索引
+        int index = 0;
+        // 遍历计数数组，将计数数组的索引填充到结果数组中
+        for (int i=0; i<count.length; i++) {
+            while (count[i]>0) {
+                result[index++] = i;
+                count[i]--;
+            }
+        }
+        // 返回结果数组
+        return result;
+    }
 }
