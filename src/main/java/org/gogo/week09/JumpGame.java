712cc6a012fab17770674ff0d50331a3a07f05da
Redick01 <11090829@qq.com>
2021-08-05 23:20:14 +0800

    jump game

---
+++ src/main/java/org/gogo/week09/JumpGame.java
@@ -0,0 +1,43 @@
+package org.gogo.week09;
+
+/**
+ * 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 判断你是否能够到达最后一个下标。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/jump-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author Redick
+ * @date 2021/8/5 11:09 下午
+ */
+public class JumpGame {
+
+    /**
+     * 贪心算法，从后往前贪心
+     * @param nums
+     * @return
+     */
+    public boolean plan1(int[] nums) {
+        if (null == nums) {
+            return false;
+        }
+        int endReachable = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] + i >= endReachable) {
+                endReachable = i;
+            }
+        }
+        return endReachable == 0;
+    }
+}
