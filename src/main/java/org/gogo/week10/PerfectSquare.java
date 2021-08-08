f3ca2ff336c691086897a92904786290fc7b33b8
Redick01 <11090829@qq.com>
2021-08-08 23:57:36 +0800

    erfen

---
+++ src/main/java/org/gogo/week10/PerfectSquare.java
@@ -0,0 +1,38 @@
+package org.gogo.week10;
+
+/**
+ * 有效的完全平方数
+ * 给定一个 正整数 num ，编写一个函数，如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
+ *
+ * 进阶：不要 使用任何内置的库函数，如  sqrt 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：num = 16
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：num = 14
+ * 输出：false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-perfect-square
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author Redick
+ * @date 2021/8/8 11:45 下午
+ */
+public class PerfectSquare {
+
+    public boolean isPerfectSquare(int num) {
+        if (0 == num || 1 == num) {
+            return true;
+        }
+        int left = 1, right = num;
+        while (left < right) {
+
+        }
+        return true;
+    }
+}
