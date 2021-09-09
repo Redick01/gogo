4b6057b1900c42de44012d6761ac64b07cb88536
Redick01 <11090829@qq.com>
2021-09-09 23:28:32 +0800

    reverse bits

--- src/main/java/org/gogo/week12/ReverseBits.java
+++ src/main/java/org/gogo/week12/ReverseBits.java
@@ -1,5 +1,7 @@
 package org.gogo.week12;
 
+import java.util.Stack;
+
 /**
  * @author Redick
  * @date 2021/9/8 11:40 下午
@@ -7,6 +9,13 @@ package org.gogo.week12;
 public class ReverseBits {
 
     public int reverseBits(int n) {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            // 从低到高的枚举n的每一位，并将其添加到result中
+            result |= (n & 1) << (31 - i);
+            // 每次操作完右移一位
+            n >>>= 1;
+        }
+        return result;
     }
 }
